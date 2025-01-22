5041ec8d1eff11f7e56d7f5ff0ab68fd81d8b202
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2025-01-22 12:37:56 +0700

    chore: init basic mvc project structure

---
+++ src/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const CANVAS = document.getElementById("sofa-canvas");
+  const CTX = CANVAS.getContext("2d");
+  const START_BUTTON = document.getElementById("btn-start");
+  START_BUTTON.addEventListener("click", () => {});
+});
